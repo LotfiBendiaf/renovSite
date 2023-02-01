1f16ae84ca86a125628383353522fafaec39720d
Lotfi <lotfi.m.bendiaf@gmail.com>
2023-02-01 22:46:32 +0100

    Animations added

--- static/Scripts/script.js
+++ static/Scripts/script.js
@@ -59,3 +59,38 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+// Fade in & Sliders
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+let theme = localStorage.getItem('theme')
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+  };
+
+const appearOnScroll = new IntersectionObserver 
+  (function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target)
+      }
+    })
+  }, 
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  })
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider)
+  })
